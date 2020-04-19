7f88829f765328b0507fa5251ac66aba09acd9de
Souvik <souvikde.ns@gmail.com>
2020-04-19 20:12:37 +0530

    json to csv format converted

--- test/test.js
+++ test/test.js
@@ -1,3 +1,11 @@
+const {prepareTable} = require('../index')
+
 test('Fake Test',()=>{
     expect(true).toBeTruthy()
+})
+
+test('table preparation',()=>{
+    const param = [{name: "Souvik",age: 20},{name: "Shounak",age: 15},{code: "Java"}]
+    const val = prepareTable(param)
+    expect(val).toEqual([['name','age','code'],[['Souvik',20,''],['Shounak',15,''],['','','Java']]])
 })
